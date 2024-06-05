08a54a49c94100b0cd290c3170207308429c9433
Neema-Charity <ncharityyy@gmail.com>
2024-06-05 22:18:22 +0300

    lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 5, 'M', 'White', 'calm', True),
+    ('Rowdy', 2, 'M', 'Black', 'aggressive', True),
+    ('Tabitha', 3, 'F', 'Brown', 'calm', True),
+    ('Sergeant Brown', 1, 'M', 'Brown', 'aggressive', True),
+    ('Melissa', 4, 'F', 'Grey', 'calm', True),
+    ('Grinch', 3, 'M', 'Nude', 'aggressive', True),
+    ('Wendy', 2, 'F', 'Peach', 'calm', True),
+    (NULL, 1, 'M', 'White', 'calm', True);
